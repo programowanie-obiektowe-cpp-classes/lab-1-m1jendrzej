cf37b4a51f47992c7351c68e32b6757bbf161a57
m1jendrzej <mjendrzej03@gmail.com>
2025-10-19 15:43:30 +0200

    Rozwiazanie zadania

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,38 @@
 class Wektor2D
 {
     // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D()
+    {
+        x_ = 0;
+        y_ = 0;
+    }
+
+    Wektor2D(double a, double b)
+    {
+        x_ = a;
+        y_ = b;
+    }
+
+    void setX(double a) { x_ = a; };
+
+    void setY(double a) { y_ = a; }
+
+    double getX() { return x_; };
+
+    double getY() { return y_; };
+
+    Wektor2D operator+(const Wektor2D& vector2)
+    {
+        return Wektor2D{x_ + vector2.x_, y_ + vector2.y_};
+    }
+
+    double operator*(const Wektor2D& vector2) 
+    { 
+        return x_ * vector2.x_ + y_ * vector2.y_; 
+    }
+
+private:
+    double x_;
+    double y_;
 };
